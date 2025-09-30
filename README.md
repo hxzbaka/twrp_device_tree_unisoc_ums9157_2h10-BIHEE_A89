eaf22c6b966bc0cad4ac1e039e85a21c93defb90
hxzbaka <3606189689@qq.com>
2025-10-01 07:02:42 +0800

    up1

---
+++ README.md
@@ -0,0 +1,26 @@
+# TWRP device tree for BIHEE A89
+BIHEE A89 (codenamed _"ums9157_2h10"_) 
+![BIHEE_A89](https://ts1.tc.mm.bing.net/th/id/OIP-C._WayArHro38qB6xjT31ydwHaHa?w=400&h=400)
+## Device specifications
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+SoC     | UNISOC T157
+CPU     | 1x 3.35 GHz Cortex-X3, 3x 3.0 GHz Cortex-A715, 4x 2.0 GHz Cortex-A510
+GPU     | Immortalis-G715 MC11
+Memory  | 3/4 GB
+Shipped Android Version | 13
+Storage | 32/64 GB
+Battery | 3000 mAh, Li-Polymer
+
+## Features
+
+Works:
+- [X] ADB
+- [X] Display
+- [X] Touch 
+- [X] Fasbootd
+- [X] USB OTG
+
+## Donate me
+![test]()
---
+++ device.mk
@@ -0,0 +1,110 @@
+#
+# Copyright (C) 2022 The TWRP Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/unisoc/ums9157_2h10
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 33
+PRODUCT_TARGET_VNDK_VERSION := 33
+
+# A/B
+AB_OTA_UPDATER := true
+ENABLE_VIRTUAL_AB := true
+TARGET_ENFORCE_AB_OTA_PARTITION_LIST := true
+AB_OTA_PARTITIONS += \
+    boot \
+    ch_sys \
+    dtbo \
+    l_agdsp \
+    mmcblk0boot1 \
+    nr_deltanv \
+    nr_fixnv1 \
+    nr_fixnv2 \
+    nr_modem \
+    nr_phy \
+    odm \
+    pm_sys \
+    product \
+    sdc \
+    sml \
+    system \
+    system_ext \
+    teecfg \
+    trustos \
+    uboot \
+    vbmeta \
+    vbmeta_odm \
+    vbmeta_product \
+    vbmeta_system \
+    vbmeta_system_ext \
+    vbmeta_vendor \
+    vendor \
+    vendor_boot \
+    vendor_dlkm
+    
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+    checkpoint_gc
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    FILESYSTEM_TYPE_system=erofs \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=erofs \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+# Additional Target Libraries
+TARGET_RECOVERY_DEVICE_MODULES += \
+    android.hardware.graphics.common@1.0 \
+    libion \
+    libxml2
+
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.graphics.common@1.0.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
+
+# Bootctrl
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctrl
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Drm
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.4
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
+# Hidl Service
+PRODUCT_ENFORCE_VINTF_MANIFEST := true
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
